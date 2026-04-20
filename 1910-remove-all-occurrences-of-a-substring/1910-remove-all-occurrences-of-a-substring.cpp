c9973997eae2c172c5855cb78749c68ffbdb483d
PragyanT23 <pragyantiwari70@gmail.com>
2026-04-20 11:24:21 +0530

    Time: 0 ms (100%), Space: 9.2 MB (92.17%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        
+        // string ans;
+        // for(char c:s){
+        //     ans+=c;
+        //     if(ans.size()>=part.size()){
+        //         int pos=ans.find(part);
+        //         if(pos!=string::npos){
+        //             ans.erase(pos,part.length());
+        //         }
+        //     }
+        // }
+        // return ans;
+    while(s.find(part)!=string::npos){
+                int pos=s.find(part);
+                if(pos!=string::npos){
+                    s.erase(pos,part.length());
+                }
+    }
+            return s;
     }
 };
